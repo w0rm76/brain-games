65da120d2b2cf2419b7aba823b7b7b88aacc41ee
Alexey Vorobev <chir_10@mail.ru>
2024-06-27 09:51:32 +0200

    fix typo

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeteing = () => {
+export const greeting = () => {
     const name = readlineSync.question('May I have your name?');
     console.log(`Hello, ${name}!`);
 }
