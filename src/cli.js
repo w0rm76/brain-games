d70be1e23ba7b94fd9295ecc007db24cf92cb02c
Alexey Vorobev <chir_10@mail.ru>
2024-06-27 11:35:21 +0200

    fix after linter check

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
 export default greeting;
