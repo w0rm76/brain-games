3eff2e4648119a716809b993b6ca3b9d148cfc97
Alexey Vorobev <chir_10@mail.ru>
2024-06-27 09:54:54 +0200

    fix greeting message

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
     const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}! `);
+    console.log(`Hello, ${name}!`);
 }
 
 export default greeting;
