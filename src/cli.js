87516e0cd00fb0be846f02d0e1b9a697db2f605e
Alexey Vorobev <chir_10@mail.ru>
2024-06-27 09:53:11 +0200

    add export default

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
     const name = readlineSync.question('May I have your name?');
     console.log(`Hello, ${name}!`);
 }
+
+export default greeting;
