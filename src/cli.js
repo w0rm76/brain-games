48a656020a767be498fd2f661fc1262cc014cf92
Alexey Vorobev <chir_10@mail.ru>
2024-06-27 09:36:33 +0200

    add greeting

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const greeteing = () => {
+    const name = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${name}!`);
+}
