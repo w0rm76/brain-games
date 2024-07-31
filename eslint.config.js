96ba4be490c7a68e57a544824eeedeeabd4110b9
Alexey Vorobev <chir_10@mail.ru>
2024-07-31 16:01:30 +0200

    fix eslint config error

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,31 @@
-import globals from "globals";
+import globals from 'globals';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("airbnb"),
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      parserOptions: {
+        // Required for certain syntax usages
+        ecmaVersion: 2020,
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('airbnb'),
   {
     rules: {
       'no-underscore-dangle': [
